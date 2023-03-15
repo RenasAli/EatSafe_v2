b4fae0f18f5672a08c9fe21e282e07a1f95573d8
RenasAli <rena0506@stud.kea.dk>
2023-03-15 15:11:21 +0100

    .

--- app/src/main/java/com/example/eatsafe_v2/UnsafeActivity.kt
+++ app/src/main/java/com/example/eatsafe_v2/UnsafeActivity.kt
@@ -1,11 +1,38 @@
 package com.example.eatsafe_v2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class UnsafeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_unsafe)
+        bottomNavigationView()
+    }
+    fun bottomNavigationView(){
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menuScanner -> {
+                    val menuScannerIntent = Intent(this,MainActivity::class.java)
+                    startActivity(menuScannerIntent)
+                    true
+                }
+                R.id.MenuGroups -> {
+                    val menuGroupsIntent = Intent(this,GroupsActivity::class.java)
+                    startActivity(menuGroupsIntent)
+                    true
+                }
+                R.id.menuWallet -> {
+                    val menuWalletIntent = Intent(this,WalletActivity::class.java)
+                    startActivity(menuWalletIntent)
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 }
